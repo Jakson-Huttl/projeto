7ca80e78cd6fe13f4fb6ef6d004d4b275c3f2f12
Jakson Huttl <jakson.huttl@gmail.com>
2023-09-17 22:37:42 -0300

    nova atualização imagens

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function togglemode() {
     // substituir a imagem
     if (html.classList.contains("light")) {
         // se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("src", "assets/avatar-light.png")
     } else {
         // se tiver sem light mode, manter a foto normal
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "assets/avatar.png")
     }
 
 }
